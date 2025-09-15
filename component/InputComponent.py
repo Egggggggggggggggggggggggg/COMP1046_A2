b5e12846a4e026ae06b5fcf9af08334e244a761d
PratikSapkota01 <Sapkotapratik098@gmail.com>
2025-09-15 16:15:56 +0930

    input compoments code is done

---
+++ component/InputComponent.py
@@ -0,0 +1,11 @@
+from component.Component import Component
+from abc import ABC
+
+class InputComponent(Component, ABC):
+    def __init__(self, name: str, voltage: float, price: float) -> None:
+        super().__init__(name, price)
+        self.__voltage = voltage
+
+    @property
+    def voltage(self) -> float:
+        return self.__voltage
