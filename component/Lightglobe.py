a08f8eea136a3b65870293f11c9cc7553470d280
Unubileg Adilbish <523127@learning.eynesbury.edu.au>
2025-09-15 16:40:17 +0930

    methods for lightglobe class

--- component/Lightglobe.py
+++ component/Lightglobe.py
@@ -4,4 +4,10 @@ class Lightglobe(Light):
     def __init__(self, name: str, price: float, colour: str, voltage: float, current: float) -> None:
         super().__init__(name, price, colour, voltage, current)
         return None
+    
+    def showDetails(self) -> str:
+        return "LightGlobe -> " + super().showDetails()
+    
+    def duplicate(self) -> "Lightglobe":
+        return Lightglobe(self.name, self.price, self.colour, self.voltage, self.current)
     pass
