c346194604e97041f4c62b7a9bb859de75f3756d
PratikSapkota01 <Sapkotapratik098@gmail.com>
2025-09-15 16:45:36 +0930

    buzzer code is done and make the changes on the functions

--- component/Buzzer.py
+++ component/Buzzer.py
@@ -1,14 +1,20 @@
-
-
-from component.OutputComponent import OutputComponent
 from component.Component import Component
 
-class Buzzer(OutputComponent):
-    def __init__(self, frequency: float, sound_pressure: float, voltage: float, current: float, price: float) -> None:
-        super().__init__("Buzzer", voltage, price)
+class Buzzer(Component):
+    def __init__(self, voltage: float, current: float, frequency: float, sound_pressure: float, price: float) -> None:
+        super().__init__("Buzzer", price)
+        self.__voltage = voltage
+        self.__current = current
         self.__frequency = frequency
         self.__sound_pressure = sound_pressure
-        self.__current = current
+
+    @property
+    def voltage(self) -> float:
+        return self.__voltage
+
+    @property
+    def current(self) -> float:
+        return self.__current
 
     @property
     def frequency(self) -> float:
@@ -18,28 +24,25 @@ class Buzzer(OutputComponent):
     def sound_pressure(self) -> float:
         return self.__sound_pressure
 
-    @property
-    def current(self) -> float:
-        return self.__current
+    def calculate_wattage(self) -> float:
+        return self.voltage * self.current
+
+    def showDetails(self) -> str:
+        return self.toString()
 
     def duplicate(self) -> "Buzzer":
-        return Buzzer(self.frequency, self.sound_pressure, self.voltage, self.current, self.price)
+        return Buzzer(self.voltage, self.current, self.frequency, self.sound_pressure, self.price)
 
     @classmethod
-    def from_string(cls, data: str) -> "Buzzer":
+    def fromString(cls, data: str) -> "Buzzer":
         parts = data.split(",")
         return cls(float(parts[0]), float(parts[1]), float(parts[2]), float(parts[3]), float(parts[4]))
 
-    def to_csv(self) -> str:
-        return str(self.frequency) + "," + str(self.sound_pressure) + "," + str(self.voltage) + "," + str(self.current) + "," + str(self.price)
+    def toCSV(self) -> str:
+        return str(self.voltage) + "," + str(self.current) + "," + str(self.frequency) + "," + str(self.sound_pressure) + "," + str(self.price)
 
-    def to_string(self) -> str:
+    def toString(self) -> str:
         return str(self.voltage) + "V " + str(self.current) + "mA " + str(self.frequency) + "Hz " + str(self.sound_pressure) + "dB Buzzer $" + str(self.price)
 
-    def is_equals(self, other: "Component") -> bool:
-        return (
-            isinstance(other, Buzzer)
-            and self.frequency == other.frequency
-            and self.voltage == other.voltage
-            and self.current == other.current
-        )
+    def isEquals(self, other: "Component") -> bool:
+        return isinstance(other, Buzzer) and self.voltage == other.voltage and self.current == other.current and self.frequency == other.frequency and self.sound_pressure == other.sound_pressure
