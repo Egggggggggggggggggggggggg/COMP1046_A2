61dd245ca1b5cc2a0f400734dc0219d0b837d3cc
Unubileg Adilbish <523127@learning.eynesbury.edu.au>
2025-09-15 16:34:47 +0930

    class lightglobe initializers

--- component/Lightglobe.py
+++ component/Lightglobe.py
@@ -1,4 +1,7 @@
 from component.Light import Light
 
 class Lightglobe(Light):
+    def __init__(self, name: str, price: float, colour: str, voltage: float, current: float) -> None:
+        super().__init__(name, price, colour, voltage, current)
+        return None
     pass
