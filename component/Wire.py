e4b390b04d672d063b68582c3d32f6f5af0ad023
Egggggggggggggggggggggggg <521560@learning.eynesbury.edu.au>
2025-09-15 16:22:42 +0930

    fix wire class file

--- component/Wire.py
+++ component/Wire.py
@@ -1,7 +1,6 @@
 from component.Component import Component
 
 class Wire(Component):
-<<<<<<< HEAD
     def __init__(self, name: str, price: float, length: float) -> None:
         super().__init__(name, price)
         self.length = length
@@ -26,29 +25,3 @@ class Wire(Component):
                self.name == other.name and \
                self.price == other.price and \
                self.length == other.length
-=======
-    def __init__(self, length: float, price: float) -> None:
-        super().__init__("Wire", price)
-        self.__length = length
-
-    @property
-    def length(self) -> float:
-        return self.__length
-
-    def duplicate(self) -> "Wire":
-        return Wire(self.length, self.price)
-
-    @classmethod
-    def from_string(cls, data: str) -> "Wire":
-        parts = data.split(",")
-        return cls(float(parts[0]), float(parts[1]))
-
-    def to_csv(self) -> str:
-        return str(self.length) + "," + str(self.price)
-
-    def to_string(self) -> str:
-        return str(self.length) + "mm Wire $" + str(self.price)
-
-    def is_equals(self, other: "Component") -> bool:
-        return isinstance(other, Wire) and self.length == other.length
->>>>>>> 469e8e68d6b8f34a1a911cad2c9ccff187ffa0e2
